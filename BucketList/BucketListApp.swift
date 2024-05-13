e023bbb5ea49f45c6f3bd062834992bf41413331
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-05-13 18:40:49 +0200

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Carlos Román Alcaide on 13/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
