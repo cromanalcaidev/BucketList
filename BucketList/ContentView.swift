b3bb0842519a671496b6410f4e9d9854afdd9576
Carlos Román Alcaide <cromanalcaidev@gmail.com>
2024-05-21 17:56:49 +0200

    Day 73, pj. 14, pt. 6
    - Maps implemented
    - Comms with Wikipedia implemented
    - MVVM arch. implemented

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,49 @@
 //  Created by Carlos Román Alcaide on 13/5/24.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 56, longitude: -3),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
+    
+    @State private var viewModel = ViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapReader { proxy in
+                Map(initialPosition: startPosition) {
+                    ForEach(viewModel.locations) { location in
+                        Annotation(location.name, coordinate: location.coordinate) {
+                            Image(systemName: "star.circle")
+                                .resizable()
+                                .foregroundStyle(.red)
+                                .frame(width: 44, height: 44)
+                                .background(.white)
+                                .clipShape(.circle)
+                                .onLongPressGesture {
+                                    viewModel.selectedPlace = location
+                                }
+                        }
+                    }
+                }
+                .onTapGesture { position in
+                    if let coordinate = proxy.convert(position, from: .local) {
+                        viewModel.addLocation(at: coordinate)
+                    }
+                }
+                .sheet(item: $viewModel.selectedPlace) { place in
+                    EditView(location: place) { newLocation in
+                        viewModel.updateLocation(location: newLocation)
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
